81c6690876c373471d76340e8eee76af15ab77b9
Moufid17 <moufid.mtr@gmail.com>
2024-12-18 15:46:27 +0100

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,31 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Cuisine-connect',
   description: "Ce site propose une expérience de recherche avancée pour les recettes, grâce à l'intégration de l'intelligence artificielle. Les utilisateurs peuvent trouver des recettes en fonction de mots-clés, chaque recette étant accompagnée d'une description, de la liste des ingrédients, de suggestions d'accompagnements générés par IA, et parfois de recettes similaires. Un chatbot est disponible pour répondre aux questions sur les recettes, et les utilisateurs connectés peuvent personnaliser leur expérience en ajoutant leurs allergies et en notant les recettes.",
+  applicationName: 'Cuisine-connect',
+  openGraph: {
+    type: "website",
+    title: "Cuisine-connect",
+    description: "Vos recettes préférées à portée de main",
+    siteName: "Cuisine connect",
+    locale: "fr_FR",
+    url: "https://cuisine-connect.vercel.app",
+    images: {
+      url: "/appIcon.svg",
+      alt: "Cuisine-connect",
+      width: 1200,
+      height: 630
+    }
+  },
+  authors: {
+    name: "Moufid Moutarou",
+  },
+  creator: "Moufid Moutarou",
+  generator: "Next.js",
+  keywords: ["Recettes", "Cuisine", "IA", "Chatbot", "personnalisation"],
+  icons: {
+    favicon: "/appIcon.svg",
+    appleTouchIcon: "/appIcon.svg",
+  }
 } 
 
 export default async function RootLayout({ children,}: {  children: React.ReactNode}) {
